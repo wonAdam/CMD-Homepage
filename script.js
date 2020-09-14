61867cefe5e1219509c21af83e58a7b8264d990f
AdamWon <ehdgus5500@gmail.com>
2020-09-14 23:54:00 +0900

    caret

--- script.js
+++ script.js
@@ -1,36 +1,52 @@
 const input = document.querySelector('.cmd-input');
+const caret = document.querySelector('.caret')
 const cmdWindow = document.querySelector('#window-content');
 const githubAnchor = document.querySelector('.github > a');
 const instaAnchor = document.querySelector('.insta > a');
 const youtubeAnchor = document.querySelector('.youtube > a');
 const solvedAnchor = document.querySelector('.solved > a');
+const dontPrint = ['Control', 'Alt', 'Shift', 'CapsLock', 'Tab', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'F13', 'BrowserHome', 'ContextMenu', 'ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown', 'Insert', 'Home', 'PageUp', 'Delete', 'End', 'PageDown', 'ScrollLock', 'Pause', 'Escape', 'OS', 'Meta'];
+input.textContent = "";
+let command = "";
 
 
-
-
-cmdWindow.addEventListener('click', (e) => {
-    input.focus();
-})
-input.addEventListener("keyup", function(event) {
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13) {
-      // Cancel the default action, if needed
-      const inputContent = input.innerText.trim().toLowerCase();
-      console.log(inputContent)
-      input.innerText = "";
-
-        if(inputContent === 'github'){
-            githubAnchor.click();
-            console.log(inputContent)
-        }
-        else if(inputContent === 'instagram'){
-            instaAnchor.click();
-        }
-        else if(inputContent === 'youtube'){
-            youtubeAnchor.click();
-        }
-        else if(inputContent === 'solved.ac'){
-            solvedAnchor.click();
-        }
+document.body.onkeydown = (event) => {
+    if(dontPrint.includes(event.key)){
+        return;
     }
-})
+    else if(event.key === 'Backspace'){
+        command = command.substring(0, command.length-1);
+        input.textContent = command;
+        return;
+    }
+    else if(event.key === 'Enter'){
+        if(command === 'github')
+            githubAnchor.click();
+        else if(command === 'instagram')
+            instaAnchor.click();
+        else if(command === 'youtube')
+            youtubeAnchor.click();
+        else if(command === 'solved.ac')
+            solvedAnchor.click();
+
+        command = "";
+        input.textContent = command;    
+        return;
+    }
+
+    command += event.key;
+    input.textContent = command;
+    
+}
+
+let blink = true;
+setInterval(() => {
+    if(blink){
+        caret.style.backgroundColor = "#aaa";
+        blink = false;
+    }
+    else{
+        caret.style.backgroundColor = "#000";
+        blink = true;
+    }
+}, 600)
