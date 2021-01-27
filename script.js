f30d3d169fb939bd46bf21a896c23b3abe756010
Won Dong Hyun <50472273+wonAdam@users.noreply.github.com>
2021-01-27 23:36:16 +0900

    notion added to js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const cmdWindow = document.querySelector('#window-content');
 const githubAnchor = document.querySelector('.github > a');
 const instaAnchor = document.querySelector('.insta > a');
 const youtubeAnchor = document.querySelector('.youtube > a');
+const notionAnchor = document.querySelector('.notion > a');
 const solvedAnchor = document.querySelector('.solved > a');
 const dontPrint = ['Control', 'Alt', 'Shift', 'CapsLock', 'Tab', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10', 'F11', 'F12', 'F13', 'BrowserHome', 'ContextMenu', 'ArrowLeft', 'ArrowUp', 'ArrowRight', 'ArrowDown', 'Insert', 'Home', 'PageUp', 'Delete', 'End', 'PageDown', 'ScrollLock', 'Pause', 'Escape', 'OS', 'Meta'];
 input.textContent = "";
@@ -26,6 +27,8 @@ document.body.onkeydown = (event) => {
             instaAnchor.click();
         else if(command === 'youtube')
             youtubeAnchor.click();
+        else if(command === 'notion')
+            notionAnchor.click();
         else if(command === 'solved.ac')
             solvedAnchor.click();
 
